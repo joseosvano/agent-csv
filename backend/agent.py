ae0c262a48197772f94722c9ec88ee6e7def5a94
Jose Osvano <jose.osvano.ext@sascar.com.br>
2025-09-29 15:33:53 -0300

    Retirada do handle_parsing_errors
    Retirada do handle_parsing_errors

--- backend/agent.py
+++ backend/agent.py
@@ -32,7 +32,7 @@ class CSVAnalysisAgent:
                 df=self.df,
                 llm=self.llm,
                 verbose=True,
-                agent_executor_kwargs={"memory": self.memory, "handle_parsing_errors": True},
+                agent_executor_kwargs={"memory": self.memory},
                 allow_dangerous_code=True
             )
             return True
